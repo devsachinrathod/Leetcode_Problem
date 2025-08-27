7f17715fcced29254897a020d11e6a7256511cf6
Sachin Rathod <sr5631080@gmail.com>
2025-08-27 23:43:36 +0530

    class

---
+++ class.js
@@ -0,0 +1,13 @@
+
+class Animal {
+  constructor(name, legCount) {
+    this.name = name
+    this.legCount = legCount
+  }
+  describe() {
+    return `${this.name} has ${this.legCount} legs`
+  }
+}
+
+const a2 = new Animal('sasda', "raffa");
+console.log(a2.describe());
