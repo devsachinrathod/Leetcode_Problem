845e4f9b322636ec0546c2c79d26519e1af6bb9b
Sachin Rathod <sr5631080@gmail.com>
2025-08-27 13:34:49 +0530

    add

--- json.js
+++ json.js
@@ -1,21 +1,27 @@
-function jsonMethods(jsonString) {
+function jsonMethods(jsonString, anotherString) {
   console.log("Original JSON String:", jsonString);
 
   // Parsing JSON string to JavaScript object
-  let parsedObject = JSON.parse(jsonString);
-  console.log("After JSON.parse():", parsedObject);
+  setTimeout(function() {
+    let parsedObject = JSON.parse(jsonString, anotherString);
+    console.log("After JSON.parse():", parsedObject);
 
-// //   Stringifying JavaScript object to JSON string
-//   let jsonStringified = JSON.stringify(parsedObject);
-//   console.log("After JSON.stringify():", jsonStringified);
-
-let Sringfyingdata = JSON.stringify(parsedObject);
-console.log("After JSON.stringify():", Sringfyingdata);
+    // Stringifying JavaScript object to JSON string
+    let Sringfyingdata = JSON.stringify(parsedObject);
+    console.log("After JSON.stringify():", Sringfyingdata);
+  }, 3000);
 }
 
 
 // Example Usage for JSON Methods
-const sampleJSONString =
-  '{"name": "John", "age": 30, "city": "New York"}';
-
+const sampleJSONString = '{"name": "John", "age": 30, "city": "New York"}';
 jsonMethods(sampleJSONString);
+
+
+const anotherString = function (key, value) {
+    // Exclude properties with key "age"
+    if (key === "age") {
+        return undefined;
+    }
+    return value;
+}
