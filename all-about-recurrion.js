921758841e8e4ca8834a303d803d187cead8c83b
Sachin Rathod <sr5631080@gmail.com>
2025-08-30 20:46:37 +0530

    all about recurion

---
+++ all-about-recurrion.js
@@ -0,0 +1,33 @@
+// 01-all-recurion-programs.js
+
+// Find the facotorial of numbers
+
+function fact(n){
+    if(n==1){
+       return n;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
+console.log(fact(5));
+
+function printNu( num){
+  if (num == 0){
+    console.log(num);
+  }
+  else{
+     printNu(num - 1);
+  }
+}
+console.log(printNu(10));
+function printNu(num) {
+  if (num === 0) {
+    return; // base case, stop recursion
+  }
+  printNu(num - 1);   // recursive call first
+  console.log(num);   // print after recursion
+}
+
+printNu(10);
+
