7f9ad4ec8f910bd1315251d8e257ec3b19018fef
Sachin Rathod <sr5631080@gmail.com>
2025-09-01 18:59:08 +0530

    first commit

--- promise_delay.js
+++ promise_delay.js
@@ -1,47 +1,72 @@
 
-// function wait(){
-//   return  new Promise ((resolve)=>{
-//     setInterval(()=>{
-// resolve("this resolved");
-//     },1000)
+// // function wait(){
+// //   return  new Promise ((resolve)=>{
+// //     setInterval(()=>{
+// // resolve("this resolved");
+// //     },1000)
     
+// //   })
+
+// // }
+// // let rsukt = wait();
+// // function printHello() {
+// //   return new Promise((resolve) => {
+// //     setTimeout(() => {
+// //       console.log("f1");
+// //       resolve(); // signal that f1 is done
+// //     }, 2000);
+// //   });
+// // }
+
+// // async function main() {
+// //   await printHello();   // wait until f1 is done
+// //   console.log("f2");    // then run f2
+// // }
+
+// // main();
+
+// function printUsingLoop(){
+//   return new Promise((resolve)=>{
+//     setTimeout(()=>{
+//       resolve("Fetch data");
+//     },2000)
 //   })
-
 // }
-// let rsukt = wait();
-// function printHello() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       console.log("f1");
-//       resolve(); // signal that f1 is done
-//     }, 2000);
-//   });
-// }
-
-// async function main() {
-//   await printHello();   // wait until f1 is done
-//   console.log("f2");    // then run f2
-// }
-
-// main();
-
-function printUsingLoop(){
-  return new Promise((resolve)=>{
-    setTimeout(()=>{
-      resolve("Fetch data");
-    },2000)
-  })
-}
-// console.log("start");
-// printUsingLoop().then((result)=>{
-//   console.log("all done", result);
-// })
+// // console.log("start");
+// // printUsingLoop().then((result)=>{
+// //   console.log("all done", result);
+// // })
  
-async function f1(result){
-  await printUsingLoop().then((rea)=>{
-console.log("data is fetched", rea);
-  });
+// async function f1(result){
+//   await printUsingLoop().then((rea)=>{
+// console.log("data is fetched", rea);
+//   });
   
+// }
+
+// f1();
+
+// Each function returns a promise that resolves after given seconds
+function wait1(t) {
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
-f1();
+function wait2(t) {
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
+}
+
+function wait3(t) {
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
+}
+
+// Sequentially call wait1, wait2, wait3 and return total time in ms
+function calculateTime(t1, t2, t3) {
+  const start = Date.now(); // start time in milliseconds
+
+  return wait1(t1)
+    .then(() => wait2(t2))
+    .then(() => wait3(t3))
+    .then(() => Date.now() - start); // total time in ms
+}
+
+module.exports = calculateTime;
