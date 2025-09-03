104be0f687ac94c34f2d25e96fe2347976317a86
Sachin Rathod <sr5631080@gmail.com>
2025-09-03 22:16:03 +0530

    add some string function

--- all_number-methods.js
+++ all_number-methods.js
@@ -1,21 +1,39 @@
-function explainParseInt(value) {
-  console.log("Original Value:", value);
-  let result = parseInt(value);
-  console.log("After parseInt:", result);
+// function explainParseInt(value) {
+//   console.log("Original Value:", value);
+//   let result = parseInt(value);
+//   console.log("After parseInt:", result);
+// }
+// explainParseInt("11sachin23.45");
+// explainParseInt("42");
+// explainParseInt("42px");
+// explainParseInt("3.14");
+
+
+// function explainParseFloat(value) {
+//   console.log("Original Value:", value);
+//   let result = parseFloat(value);
+//   console.log("After parseFloat:", result);
+// }
+
+// // Example Usage for parseFloat
+// explainParseFloat("3.14");
+// explainParseFloat("42");
+// explainParseFloat("42px");
+
+// let arr = [2,4,5,6];
+// function getInputasString(valus){
+//   console.log("Before Trim :",valus);
+//   const trimFun = arr.trim();
+//     console.log("Trim ", trimFun);
+//   const intValue = parseInt(valus);
+
+//   console.log(valus);
+//   console.log("the valuse is ", intValue);
+// }
+
+// getInputasString("sachin");
+function trimString(str) {
+    console.log("Original String:", str);
+    console.log("After trim:", str.trim());
 }
-explainParseInt("11sachin23.45");
-explainParseInt("42");
-explainParseInt("42px");
-explainParseInt("3.14");
-
-
-function explainParseFloat(value) {
-  console.log("Original Value:", value);
-  let result = parseFloat(value);
-  console.log("After parseFloat:", result);
-}
-
-// Example Usage for parseFloat
-explainParseFloat("3.14");
-explainParseFloat("42");
-explainParseFloat("42px");
+trimString("     Hello World ");
