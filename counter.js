557399b12b0f03e164ff08c8f6edfa1d2690a550
Sachin Rathod <sr5631080@gmail.com>
2025-09-06 20:16:20 +0530

    objects-things

--- counter.js
+++ counter.js
@@ -4,13 +4,55 @@
 // }
 // counter();
 
-let num = 0;
+// let studentObj = {
+//    student1: {
+//       name: "Tanuj",
+//       surname: "rathod",
+//       class: "4th",
+//       schoolName: "Lotus high school"
+//    },
+//    student2: {
+//       name: "pavan",
+//       surname: "Jadhav",
+//       class: "3rd",
+//       schoolName: "Ram-bhau koslange school"
+//    },
+//    studentc3: {
+//       name: "Sanchika",
+//       surname: "Jadhav",
+//       class: "KG-2",
+//       schoolName: "Ram-bhau koslange school"
+//    },
+//    studentc4: {
+//       name: "Savan",
+//       surname: "Jadhav",
+//       class: "1st",
+//       schoolName: "Ram-bhau koslange school"
+//    },
+//    studentc5: {
+//       name: "Riya",
+//       surname: "Jadhav",
+//       class: "1st",
+//       schoolName: "Ram-bhau koslange school"
+//    }
+// };
 
-function countER(){
-setTimeout(()=>{
-   return num++;
-},1000);
+let studArray = ["name" , "suname", "middlename"];
+// console.log(studentObj.student1.name);
 
+const printfun = (obj) => {
+  obj.map((key) => {
+    console.log(key.name);
+  });
 }
+console.log(printfun(studArray))
+// let num = 0;
 
-console.log(countER());
+// function countER(){
+// setTimeout(()=>
+//    return num++;
+// },1000);
+
+// }
+
+// console.log(countER());
