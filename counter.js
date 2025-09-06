314f1e462732cd2e5eba68b1ce6104122e0e5fc4
Sachin Rathod <sr5631080@gmail.com>
2025-09-06 21:55:40 +0530

    add some array fun

--- counter.js
+++ counter.js
@@ -3,56 +3,80 @@
 // console.log(date.getTime());
 // }
 // counter();
+let studentObj = {
+   student1: {
+      name: "Tanuj",
+      surname: "rathod",
+      class: "4th",
+      schoolName: "Lotus high school"
+   },
+   student2: {
+      name: "pavan",
+      surname: "Jadhav",
+      class: "3rd",
+      schoolName: "Ram-bhau koslange school"
+   },
+   studentc3: {
+      name: "Sanchika",
+      surname: "Jadhav",
+      class: "KG-2",
+      schoolName: "Ram-bhau koslange school"
+   },
+   studentc4: {
+      name: "Savan",
+      surname: "Jadhav",
+      class: "1st",
+      schoolName: "Ram-bhau koslange school"
+   },
+   studentc5: {
+      name: "Riya",
+      surname: "Jadhav",
+      class: "1st",
+      schoolName: "Ram-bhau koslange school"
+   }
+};
 
-// let studentObj = {
-//    student1: {
-//       name: "Tanuj",
-//       surname: "rathod",
-//       class: "4th",
-//       schoolName: "Lotus high school"
-//    },
-//    student2: {
-//       name: "pavan",
-//       surname: "Jadhav",
-//       class: "3rd",
-//       schoolName: "Ram-bhau koslange school"
-//    },
-//    studentc3: {
-//       name: "Sanchika",
-//       surname: "Jadhav",
-//       class: "KG-2",
-//       schoolName: "Ram-bhau koslange school"
-//    },
-//    studentc4: {
-//       name: "Savan",
-//       surname: "Jadhav",
-//       class: "1st",
-//       schoolName: "Ram-bhau koslange school"
-//    },
-//    studentc5: {
-//       name: "Riya",
-//       surname: "Jadhav",
-//       class: "1st",
-//       schoolName: "Ram-bhau koslange school"
-//    }
-// };
+// Iterate over each \
+function mapObject(obj) {
+   for (let key in obj) {
+      if (obj[key].schoolName == "Ram-bhau koslange school") {
+         console.log(` ${obj[key].schoolName}`);
+      }
 
-let studArray = ["name" , "suname", "middlename"];
-// console.log(studentObj.student1.name);
+   }
+}
+
+mapObject(studentObj);
+
+
+let studArray = [
+   { name: "name" ,
+      sirname:"surname"
+   },
+   { name: "suname" },
+   { name: "middlename" }
+];
 
 const printfun = (obj) => {
-  obj.map((key) => {
-    console.log(key.name);
-  });
+  
+   obj.map((key) => {
+      if(key.sirname !== undefined){
+              console.log(key.sirname);
+         // console.log("not exist key value");
+      }
+ 
+   });
+};
+
+printfun(studArray);
+
+let num = 0;
+
+function countER(){
+   setInterval(()=>{
+      console.log(num++);
+   },100)
 }
-console.log(printfun(studArray))
-// let num = 0;
-
-// function countER(){
-// setTimeout(()=>
-//    return num++;
-// },1000);
-
-// }
+countER();
 
 // console.log(countER());
