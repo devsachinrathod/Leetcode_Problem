6792d980de53e1cf1d13e3c81cbe286719c11533
Sachin Rathod <sr5631080@gmail.com>
2025-09-06 22:22:04 +0530

    this is second question

---
+++ leetcode_problem/best-time-to-buy-stack.js
@@ -0,0 +1,14 @@
+function maxProfit(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];  // Track the minimum price so far
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;  // Calculate profit if sold today
+        }
+    }
+
+    return maxProfit;
+}
