efdfd78ae45c78b8bbb3b0f7dc0dff649951581c
Sachin Rathod <sr5631080@gmail.com>
2025-09-09 21:45:35 +0530

    ds

--- all_about_while_loop.js
+++ all_about_while_loop.js
@@ -1,12 +1,18 @@
-let arr = [4, 5, 3, 4, 5, 7];
-console.log(arr.length);
+let arr1 = [4, 5, 3, 4, 5, 7,];
+let arr2 = [4, 5, 3, 4, 5, 7];
 
-function usingWhileLoop(array) {
-    const n = array.length;
-    let i = 0;
-    while (i< n) {
-        console.log(i ,"=",array[i]);
-        i+=3;
-    }
-}
-usingWhileLoop(arr);
+let arr3 = arr1 + arr2;
+console.log(arr3);
+
+
+// console.log(arr.length);
+
+// function usingWhileLoop(array) {
+//     const n = array.length;
+//     let i = 0;
+//     while (i< n) {
+//         console.log(i ,"=",array[i]);
+//         i+=3;
+//     }
+// }
+// usingWhileLoop(arr);
