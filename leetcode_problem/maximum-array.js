b3d14528440ac601af7d01821a5f104a936b67ff
Sachin Rathod <sr5631080@gmail.com>
2025-09-10 22:44:59 +0530

    subarray

---
+++ leetcode_problem/maximum-array.js
@@ -0,0 +1,20 @@
+function findMaxAverage(nums, k) {
+    let windowSum = 0;
+    
+    // Calculate initial sum of the first window of size k
+    for (let i = 0; i < k; i++) {
+        windowSum += nums[i];
+    }
+    
+    let maxSum = windowSum;
+    
+    // Slide the window
+    for (let i = k; i < nums.length; i++) {
+        windowSum = windowSum - nums[i - k] + nums[i];
+        maxSum = Math.max(maxSum, windowSum);
+    }
+    
+    return maxSum / k;
+}
+nums = [1,12,-5,-6,50,3], k = 4
+console.log(findMaxAverage(nums,k));
