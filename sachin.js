767dbc02ff2857db13b8aa72660cdcaf29452acc
Sachin Rathod <sr5631080@gmail.com>
2025-09-14 21:08:57 +0530

    teachingjs

---
+++ sachin.js
@@ -0,0 +1,42 @@
+
+// let ans = "This is string";//string
+// let ans2 = 10;//integer
+// let ans3 = true;//boolean
+// const obj = { name:"sachin",surname:"rathod"};//object
+// let exam;//underdefine
+// let arr= [];//array
+
+// // let v1 = 5;
+// // let v2 = "5";
+
+// // console.log(v1 !== v2);
+
+
+// console.log("camparison : ",v1 == v2);
+// console.log("camparison : ",v1 === v2);
+
+
+// console.log("Arrar : ",arr);
+// console.log(exam);
+// console.log(ans2);
+// console.log(ans3);
+// console.log(obj);
+// console.log(ans);
+
+// let n1 = 10;
+// let n2 = 15;
+
+// let n3 = 30;
+// let n4 = 30;
+
+
+// console.log(n3 == n4);
+
+let a = 2;
+console.log(a);
+a++;
+console.log(a);
+
+for(let i = 0;i< 10; i++){
+    console.log(i);
+}
