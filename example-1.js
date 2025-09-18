b19d7daab096b9587a8d5938829c79ff5404e053
Sachin Rathod <sr5631080@gmail.com>
2025-09-18 19:18:04 +0530

    example.js commiit

--- example-1.js
+++ example-1.js
@@ -1,4 +1,4 @@
-  console.log( "Hello from example-1");
+console.log( "Hello from example-1");
 
 function Example_1(fun){
     console.log( "Hello from example-2",fun);   
@@ -6,6 +6,5 @@ function Example_1(fun){
 };
 function Example_2(){
         console.log( "Hello from example-3");
-
 }
-    Example_1(Example_2());
+ Example_1(Example_2());
