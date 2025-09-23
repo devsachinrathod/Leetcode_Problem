50d2a0991f7eed98f45720ca3682fb5bba4ae2c4
Sachin Rathod <sr5631080@gmail.com>
2025-09-23 18:34:29 +0530

    Practicing the recurrsion problems

--- code_for_edit.js
+++ code_for_edit.js
@@ -1,6 +1,48 @@
-for(let i = 0; i<=100; i++){
-    if(i == 50){
-        continue;
+// for(let i = 0; i<=100; i++){
+//     if(i == 50){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// function printBackTrack(num) {
+//     // let len = num.length;
+//     if (num == 20) {
+//         return 0;
+//     }
+//     console.log(num);
+//     printBackTrack(num - 1);
+// }
+// printBackTrack(100);
+
+// function realBackTracking(foots){
+//     // let n = foots.length;
+//     let calculateHighestStep = [0];
+//     if(calculateHighestStep < foots){
+//         console.log(calculateHighestStep);
+//         calculateHighestStep.push(foots);
+//         return calculateHighestStep;
+//     }
+//     realBackTracking(foots -1);
+// }
+
+// let step = [ 21 ,2,4,6,8,67];
+// console.log(realBackTracking(step));
+
+
+function realBackTracking(foots, index = 0, calculateHighestStep = []) {
+    // base case: when we reach the end
+    if (index === foots.length) {
+        return calculateHighestStep;
     }
-    console.log(i);
+
+    // check and push
+    if (foots[index] > calculateHighestStep[0] || calculateHighestStep.length === 0) {
+        calculateHighestStep[0] = foots[index];
+    }
+
+    // recursive call
+    return realBackTracking(foots, index + 1, calculateHighestStep);
 }
+let step = [21, 2, 4, 6, 8, 67];
+console.log("Highest Step:", realBackTracking(step)[0]);
--- object.js
+++ object.js
@@ -63,8 +63,31 @@
 // // mathMethods(9);
 // mathMethods(25);
 
-let v1 = 20; 
-let v2 = 30;
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr; // base case
 
-console.log(v1 > v2);
-console.log(v1 < v2);
+    let mid = Math.floor(arr.length / 2);
+
+    // recursive calls
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+    // combine step
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    let result = [];
+    let i = 0, j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i++]);
+        } else {
+            result.push(right[j++]);
+        }
+    }
+    return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+console.log(mergeSort([3, 1, 4, 2])); // [1, 2, 3, 4]
