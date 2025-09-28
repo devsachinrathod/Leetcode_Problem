a97742390a8aca26f7632d1a1818c89ef249292a
Sachin Rathod <sr5631080@gmail.com>
2025-09-29 00:05:57 +0530

    fS

--- all_about_while_loop.js
+++ all_about_while_loop.js
@@ -1,18 +1,32 @@
-let arr1 = [4, 5, 3, 4, 5, 7,];
-let arr2 = [4, 5, 3, 4, 5, 7];
+// let arr1 = [4, 5, 3, 4, 5, 7,];
+// let arr2 = [4, 5, 3, 4, 5, 7];
 
-let arr3 = arr1 + arr2;
-console.log(arr3);
+// let arr3 = arr1 + arr2;
+// console.log(arr3);
 
 
-// console.log(arr.length);
+// // console.log(arr.length);
 
-// function usingWhileLoop(array) {
-//     const n = array.length;
-//     let i = 0;
-//     while (i< n) {
-//         console.log(i ,"=",array[i]);
-//         i+=3;
-//     }
-// }
-// usingWhileLoop(arr);
+// // function usingWhileLoop(array) {
+// //     const n = array.length;
+// //     let i = 0;
+// //     while (i< n) {
+// //         console.log(i ,"=",array[i]);
+// //         i+=3;
+// //     }
+// // }
+// // usingWhileLoop(arr);
+
+
+function fS(n) {
+    let first = 0;
+    let second = 1;
+
+    for (let i = 2; i < n; i++) {
+        let c = first + second;
+        console.log(c);
+        first = second;
+        second = c;
+    }
+}
+fS(7);
--- code_for_edit.js
+++ code_for_edit.js
@@ -1,68 +1,80 @@
-// for(let i = 0; i<=100; i++){
-//     if(i == 50){
-//         continue;
+// // for(let i = 0; i<=100; i++){
+// //     if(i == 50){
+// //         continue;
+// //     }
+// //     console.log(i);
+// // }
+
+// // function printBackTrack(num) {
+// //     // let len = num.length;
+// //     if (num == 20) {
+// //         return 0;
+// //     }
+// //     console.log(num);
+// //     printBackTrack(num - 1);
+// // }
+// // printBackTrack(100);
+
+// // function realBackTracking(foots){
+// //     // let n = foots.length;
+// //     let calculateHighestStep = [0];
+// //     if(calculateHighestStep < foots){
+// //         console.log(calculateHighestStep);
+// //         calculateHighestStep.push(foots);
+// //         return calculateHighestStep;
+// //     }
+// //     realBackTracking(foots -1);
+// // }
+
+// // let step = [ 21 ,2,4,6,8,67];
+// // console.log(realBackTracking(step));
+
+
+// // function realBackTracking(foots, index = 0, calculateHighestStep = []) {
+// //     // base case: when we reach the end
+// //     if (index === foots.length) {
+// //         return calculateHighestStep;
+// //     }
+// //     // check and push
+// //     if (foots[index] > calculateHighestStep[0] || calculateHighestStep.length === 0) {
+// //         calculateHighestStep[0] = foots[index];
+// //     }
+// //     // recursive call
+// //     return realBackTracking(foots, index + 1, calculateHighestStep);
+// // }
+// // let step = [21, 2, 4, 6, 8, 67];
+// // console.log("Highest Step:", realBackTracking(step)[0]);
+// function findPaths(row, col, n, m, path, result) {
+//     if (row === n - 1 && col === m - 1) {
+//         result.push([...path]); // save path copy
+//         return;
 //     }
-//     console.log(i);
+
+//     if (row + 1 < n) {
+//         path.push("D");
+//         findPaths(row + 1, col, n, m, path, result);
+//         path.pop();
+//     }
+
+//     if (col + 1 < m) {
+//         path.push("R");
+//         findPaths(row, col + 1, n, m, path, result);
+//         path.pop();
+//     }
 // }
 
-// function printBackTrack(num) {
-//     // let len = num.length;
-//     if (num == 20) {
-//         return 0;
-//     }
-//     console.log(num);
-//     printBackTrack(num - 1);
-// }
-// printBackTrack(100);
-
-// function realBackTracking(foots){
-//     // let n = foots.length;
-//     let calculateHighestStep = [0];
-//     if(calculateHighestStep < foots){
-//         console.log(calculateHighestStep);
-//         calculateHighestStep.push(foots);
-//         return calculateHighestStep;
-//     }
-//     realBackTracking(foots -1);
-// }
-
-// let step = [ 21 ,2,4,6,8,67];
-// console.log(realBackTracking(step));
+// let result = [];
+// findPaths(0, 0, 2, 2, [], result);
+// console.log("All Paths:", result);
 
 
-// function realBackTracking(foots, index = 0, calculateHighestStep = []) {
-//     // base case: when we reach the end
-//     if (index === foots.length) {
-//         return calculateHighestStep;
-//     }
-//     // check and push
-//     if (foots[index] > calculateHighestStep[0] || calculateHighestStep.length === 0) {
-//         calculateHighestStep[0] = foots[index];
-//     }
-//     // recursive call
-//     return realBackTracking(foots, index + 1, calculateHighestStep);
-// }
-// let step = [21, 2, 4, 6, 8, 67];
-// console.log("Highest Step:", realBackTracking(step)[0]);
-function findPaths(row, col, n, m, path, result) {
-    if (row === n - 1 && col === m - 1) {
-        result.push([...path]); // save path copy
-        return;
-    }
+function a1(){
+    console.log("a1 is called");
+}
+async function a2() {
 
-    if (row + 1 < n) {
-        path.push("D");
-        findPaths(row + 1, col, n, m, path, result);
-        path.pop();
-    }
-
-    if (col + 1 < m) {
-        path.push("R");
-        findPaths(row, col + 1, n, m, path, result);
-        path.pop();
-    }
+    console.log("Hi sachin");
+    await a1();
 }
 
-let result = [];
-findPaths(0, 0, 2, 2, [], result);
-console.log("All Paths:", result);
+a2();
